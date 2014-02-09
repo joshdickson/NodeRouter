0a74a4ac2abd5aa8a772f3f07b2ae1cc9f824344
joshdickson40 <josh.dickson@wpi.edu>
2014-02-09 14:52:49 -0500

    Updated Licensing Info & Minor Refactoring
    - Added MIT license
    - Minor refactoring

--- src/noderouter/TCPLink.java
+++ src/noderouter/TCPLink.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Joshua Dickson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package noderouter;
 
 import java.io.BufferedReader;
@@ -11,6 +34,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A class that illustrates the use of a non blocking TCP linker to a front end web service 
+ * through a Node JS-based proxy server
+ * @author Joshua Dickson <josh.dickson@wpi.edu>
+ */
 public class TCPLink implements Closeable {
 	
 	private final ExecutorService executor;
@@ -19,8 +47,16 @@ public class TCPLink implements Closeable {
 	private final ServerSocket serverSocket;
 	private final Socket connectionSocket;
 	
+	/**
+	 * Run the project
+	 * @param args the port on which to open the TCP connection
+	 */
 	public static void main(String[] args) {
-		TCPLink link = new TCPLink(6514);
+		if(args.length == 0) {
+			args = new String[]{"6514"};
+		}
+		
+		TCPLink link = new TCPLink(Integer.parseInt(args[0]));
 		link.bind();
 		
 		/**
@@ -88,7 +124,8 @@ public class TCPLink implements Closeable {
 			System.out.println("Listening for incoming connection: ");
 			connectionSocketTemporary = serverSocketTemporary.accept();
 			outTemporary = new DataOutputStream(connectionSocketTemporary.getOutputStream());
-			inTemporary = new BufferedReader(new InputStreamReader(connectionSocketTemporary.getInputStream()));
+			inTemporary = new BufferedReader(new InputStreamReader(
+					connectionSocketTemporary.getInputStream()));
 		} catch(Exception ex) {
 			System.err.println("Error binding to the socket");
 			System.exit(1);
@@ -157,9 +194,6 @@ public class TCPLink implements Closeable {
 		 */
 		try {
 			executor.shutdown();
-//			executor.awaitTermination(1, TimeUnit.DAYS);
-//			connectionSocket.close();
-//			serverSocket.close();
 		} catch(Exception ex) {
 			System.err.println("There was a problem shutting down.");
 			System.exit(1);
